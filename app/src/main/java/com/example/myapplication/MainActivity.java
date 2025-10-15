d01f28f904f5e44c5399941c53d4fa69bebdf8c7
doorraa <isidora.mickovic@gmail.com>
2025-10-15 14:50:04 +0200

    settings

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> test = new HashMap<>();
+        test.put("message", "Hello Firebase!");
+
+        db.collection("test").add(test)
+                .addOnSuccessListener(documentReference ->
+                        Log.d("Firebase", "Document added!"))
+                .addOnFailureListener(e ->
+                        Log.w("Firebase", "Error adding document", e));
     }
 }
